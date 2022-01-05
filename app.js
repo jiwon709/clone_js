68bb88e4790a0e50f4c5cb8d214fa8e14f0bba54
jiwon709 <jimong8702@naver.com>
2022-01-05 15:18:54 +0900

    second exercise

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert("hi");
+//const : 바뀌지 않는 값, 상수
+const a = 5;
+const b = 2;
+const myName = "nico";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
